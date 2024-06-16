e8ca4bd0d96ebfa19864b381fb1e28c2957fbff5
Jeferson <jbasurco@unsa.edu.pe>
2024-06-15 23:55:39 -0500

    Modificando las vistas, agregando contexto y sus variables

--- django02/inicio/views.py
+++ django02/inicio/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def myHomeView(request):
-    return render(request, 'home.html',{})
+def myHomeView(request, *args, **kwargs):
+    myContext = {
+        'myText': 'Esto es sobre nosotros',
+        'myNumber': 123
+    }
+    return render(request, 'home.html', myContext)
 
 def anotherView(request):
     return HttpResponse('<h1>Solo otra página</h1>')
